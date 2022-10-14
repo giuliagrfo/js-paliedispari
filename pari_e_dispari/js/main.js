15dd23e06d5de042965c8ea7b56b065ec8f2eac1
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-14 13:25:53 +0200

    pc random number

--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,12 +1,21 @@
 // L’utente sceglie pari o dispari
-
+const userPariDispari = prompt('Scegli pari o dispari');
 
 // l'utente inserisce un numero da 1 a 5
-
+const userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
 
 // generare un numero random da 1 a 5 per il computer usando function
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(5);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
 
+
+  const pcRandomNumber = getRandomIntInclusive(1,5)
+  console.log(pcRandomNumber);
+  
 // sommare i due numeri
 
 
