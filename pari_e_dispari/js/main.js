39035c24a2ea826c1e12dfc1983aa9c2a6ae7da5
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-14 13:40:15 +0200

    sum of pc number and user number

--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,23 +1,26 @@
 // L’utente sceglie pari o dispari
 const userPariDispari = prompt('Scegli pari o dispari');
+console.log(userPariDispari);
 
 // l'utente inserisce un numero da 1 a 5
 const userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
+console.log(userNumber);
 
 // generare un numero random da 1 a 5 per il computer usando function
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(5);
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); 
   }
 
 
   const pcRandomNumber = getRandomIntInclusive(1,5)
   console.log(pcRandomNumber);
-  
-// sommare i due numeri
 
+// sommare i due numeri
+let sum = pcRandomNumber + userNumber
+console.log(sum);
 
 // stabilire se la somma dei due numeri è pari o dispari usando function
 
