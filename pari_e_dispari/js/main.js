8105fbd8df70c3a05d61acb729841af427da5a88
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-14 13:04:35 +0200

    initial commit ex-pariedispari

---
+++ pari_e_dispari/js/main.js
@@ -0,0 +1,16 @@
+// L’utente sceglie pari o dispari
+
+
+// l'utente inserisce un numero da 1 a 5
+
+
+// generare un numero random da 1 a 5 per il computer usando function
+
+
+// sommare i due numeri
+
+
+// stabilire se la somma dei due numeri è pari o dispari usando function
+
+
+// dichiarare chi ha vinto
