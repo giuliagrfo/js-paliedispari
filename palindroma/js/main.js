f7834f29a0d1eec642b76a6b724c592739db2574
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-14 12:05:18 +0200

    initial commit

---
+++ palindroma/js/main.js
@@ -0,0 +1,4 @@
+// Utilizzo prompt() per chiedere all’utente di inserire una parola
+
+
+// Creo una funzione per capire se la parola inserita è palindroma invertendo l'ordine delle lettere della stringa
