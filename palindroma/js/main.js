f73fda74079aedc28d24b52b2b113355464e0b32
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-14 12:08:31 +0200

    worde request

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,4 +1,6 @@
 // Utilizzo prompt() per chiedere all’utente di inserire una parola
-
+const userWord = prompt('Inserisci una parola per verificare se è palindroma')
+console.log(userWord); 
 
 // Creo una funzione per capire se la parola inserita è palindroma invertendo l'ordine delle lettere della stringa
+
